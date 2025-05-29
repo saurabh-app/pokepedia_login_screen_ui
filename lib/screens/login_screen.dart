f0aef0bfd949804cedf0322d24f70901dfb9499e
HP <saurkumar1994@gmail.com>
2025-05-30 00:43:42 +0530

    (SKY)login ui changes

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:pokepedia_login_screen_ui/screens/registration_screen.dart';
 import 'package:pokepedia_login_screen_ui/widgets/button.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -17,7 +18,7 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.teal,
       body: SafeArea(
         child: SingleChildScrollView(
           padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
@@ -32,14 +33,14 @@ class _LoginScreenState extends State<LoginScreen> {
                   child: Lottie.asset('assets/animations/lottie1.json'),
                 ),
               ),
-              const SizedBox(height: 20),
+              const SizedBox(height: 10),
 
               // Title
               Center(
                 child: Text(
-                  "Login",
+                  "Sign In",
                   style: TextStyle(
-                    fontSize: 26,
+                    fontSize: 35,
                     fontWeight: FontWeight.bold,
                     color: Colors.yellow[200],
                   ),
@@ -50,145 +51,155 @@ class _LoginScreenState extends State<LoginScreen> {
                 child: Text(
                   "Enter valid Email & Password",
                   style: TextStyle(
-                    fontSize: 14,
+                    fontSize: 15,
                     fontWeight: FontWeight.bold,
-                    color: Colors.teal,
+                    color: Colors.white,
                   ),
                 ),
               ),
               const SizedBox(height: 30),
               // Email Field
-              TextField(
-                controller: emailController,
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  hintText: 'Enter your email',
-                  prefixIcon: const Icon(Icons.email, color: Colors.black),
-                  suffixIcon:
-                      emailController.text.isNotEmpty
-                          ? IconButton(
-                            icon: const Icon(Icons.clear),
-                            onPressed: () {
-                              setState(() => emailController.clear());
-                            },
-                          )
-                          : null,
-                  filled: true,
-                  fillColor: Colors.white,
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: BorderSide(color: Colors.grey.shade400),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(
-                      color: Colors.yellow,
-                      width: 2,
+              Padding(
+                padding: const EdgeInsets.only(left: 20,right: 20,top: 10,bottom: 10),
+                child: TextField(
+                  controller: emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    hintText: 'Enter your email',
+                    prefixIcon: const Icon(Icons.email, color: Colors.black),
+                    suffixIcon:
+                        emailController.text.isNotEmpty
+                            ? IconButton(
+                              icon: const Icon(Icons.clear),
+                              onPressed: () {
+                                setState(() => emailController.clear());
+                              },
+                            )
+                            : null,
+                    filled: true,
+                    fillColor: Colors.teal,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(color: Colors.grey.shade400),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(
+                        color: Colors.yellow,
+                        width: 2,
+                      ),
                     ),
                   ),
+                  onChanged: (_) => setState(() {}),
                 ),
-                onChanged: (_) => setState(() {}),
               ),
               const SizedBox(height: 20),
 
               // Password Field
-              TextField(
-                controller: passwordController,
-                obscureText: !showPassword,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  hintText: 'Enter your password',
-                  prefixIcon: const Icon(Icons.lock, color: Colors.black),
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      showPassword ? Icons.visibility : Icons.visibility_off,
+              Padding(
+                padding: const EdgeInsets.only(left: 20,right: 20,top: 10,bottom: 10),
+                child: TextField(
+                  controller: passwordController,
+                  obscureText: !showPassword,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    hintText: 'Enter your password',
+                    prefixIcon: const Icon(Icons.lock, color: Colors.black),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        showPassword ? Icons.visibility : Icons.visibility_off,
+                      ),
+                      onPressed: () {
+                        setState(() => showPassword = !showPassword);
+                      },
                     ),
-                    onPressed: () {
-                      setState(() => showPassword = !showPassword);
-                    },
-                  ),
-                  filled: true,
-                  fillColor: Colors.white,
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: BorderSide(color: Colors.grey.shade400),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: const BorderSide(
-                      color: Colors.yellow,
-                      width: 2,
+                    filled: true,
+                    fillColor: Colors.teal,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide(color: Colors.grey.shade400),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(
+                        color: Colors.yellow,
+                        width: 2,
+                      ),
                     ),
                   ),
                 ),
               ),
               const SizedBox(height: 20),
 
-              // Center(
-              //   child: SizedBox(
-              //     width: 100,
-              //     height: 50,
-              //     child: ElevatedButton(
-              //       style: ElevatedButton.styleFrom(
-              //         backgroundColor: Colors.teal,
-              //         shape: RoundedRectangleBorder(
-              //           borderRadius: BorderRadius.circular(12),
-              //         ),
-              //       ),
-              //       onPressed: () {
-              //         // Handle login logic
-              //         print("Email: ${emailController.text}");
-              //         print("Password: ${passwordController.text}");
-              //       },
-              //       child: const Text(
-              //         "Login",
-              //         style: TextStyle(fontSize: 18, color: Colors.yellow),
-              //       ),
+              Center(
+                child: SizedBox(
+                  width: 100,
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.teal,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    onPressed: () {
+                      // Handle login logic
+                      print("Email: ${emailController.text}");
+                      print("Password: ${passwordController.text}");
+                    },
+                    child: const Text(
+                      "Login",
+                      style: TextStyle(fontSize: 18, color: Colors.yellow),
+                    ),
+                  ),
+                ),
+              ),
+              // LoadingAnimatedButton(
+              //   child: Text(
+              //     "Login",
+              //     style: TextStyle(
+              //       fontSize: 18,
+              //       fontWeight: FontWeight.bold,
+              //       color: Colors.yellow,
+              //     ),
+              //   ),
+              //   onTap: () => {},
+              // ),
+              const SizedBox(height: 10),
+
+              // // Forgot Password
+              // Align(
+              //   alignment: Alignment.centerRight,
+              //   child: TextButton(
+              //     onPressed: () {
+              //       // Navigate to forgot password
+              //     },
+              //     child: Text(
+              //       "Forgot Password?",
+              //       style: TextStyle(color: Colors.grey[700]),
               //     ),
               //   ),
               // ),
-              LoadingAnimatedButton(
-                child: Text(
-                  "Login",
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.yellow,
-                  ),
-                ),
-                onTap: () => {},
-              ),
-              const SizedBox(height: 10),
-
-              // Forgot Password
-              Align(
-                alignment: Alignment.centerRight,
-                child: TextButton(
-                  onPressed: () {
-                    // Navigate to forgot password
-                  },
-                  child: Text(
-                    "Forgot Password?",
-                    style: TextStyle(color: Colors.grey[700]),
-                  ),
-                ),
-              ),
 
               // Register Text
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const Text("New here? "),
-                  GestureDetector(
+                  const Text(
+                    "Don`t have an account? ",
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+                  InkWell(
                     onTap: () {
-                      // Navigate to Register screen
+                      Navigator.push(context, MaterialPageRoute(builder: (_)=>RegistrationScreen()));
                     },
-                    child: const Text(
-                      "Register",
+                    child:  Text(
+                      "Sign Up !",
                       style: TextStyle(
-                        color: Colors.redAccent,
-                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        color: Colors.yellow[300],
+                        fontWeight: FontWeight.w700,
                       ),
                     ),
                   ),
